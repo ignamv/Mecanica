f443ddf23e438df34a6bb6ad360abeafd43b721d
Ignacio Martinez Vazquez <ignamv@gmail.com>
2011-09-18 17:58:38 -0300

    Agregué potencial repulsivo, no anda porque hay ángulos prohibidos
    Agregué un potencial de la forma k/r². Al poner un k positivo el dibujo
    queda confuso porque cada trayectoria tiene un intervalo restringido de
    ángulos. El programa termina dibujando trayectorias correspondientes a
    varias condiciones iniciales.

--- orbita.py
+++ orbita.py
@@ -16,17 +16,20 @@ import pylab
 def elastico(y,t,A):
     # A = k*m/L²
     return sp.array([y[1],-y[0]+A*y[0]**(-3)])
+def prob4(y,t,A):
+    # A = 2*m*k/L²
+    return sp.array([y[1],y[0]*(-1-A)])
 def kepler(y,t,A):
     # A = G*M*m²/L² = G*M/r⁴/(dTheta/dT)²
     # Parece raro pero las m se cancelan y queda independiente de m
     return sp.array([y[1],-y[0]+A])
 
 t = sp.linspace(0,2*sp.pi,100)
-u = integrate.odeint(kepler,sp.array([1,0.2]),t,(2,))
+u = integrate.odeint(prob4,sp.array([1,0]),t,(1,))
 
 x = sp.cos(t)/u[:,0]
 y = sp.sin(t)/u[:,0]
-pylab.plot(x,y)
+pylab.scatter(x,y)
 # Marco el origen
 pylab.scatter([0],[0],marker='x')
 pylab.show()
